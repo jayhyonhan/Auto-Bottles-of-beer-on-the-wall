ff4fd3fcaa3e802470d2d85854706069ce2f6560
Japple <102459514+jayhyonhan@users.noreply.github.com>
2023-12-05 19:21:27 +0000

    done

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyperclip, keyboard
+
+def wait_for_paste():
+    while not(keyboard.is_pressed("ctrl+v")):
+        pass
+    print("paste detected!")
+    while not(keyboard.is_pressed("enter")):
+        pass
+    print("message sent!")
+
+start_int = int(input("Starting number: "))
+
+while start_int > 0:
+    pyperclip.copy(f"{start_int} bottles of beer on the wall, {start_int} of beer take one down, pass it around, {start_int-1} bottles of beer on the wall")
+    wait_for_paste()
+    start_int -= 1
